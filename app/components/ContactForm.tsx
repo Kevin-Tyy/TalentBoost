95abc24a2b36660196e37b9bbbdc4898ab8d447f
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-11-16 23:38:52 +0200

    [fix] responsiveness

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -6,34 +6,35 @@ export default function ContactForm() {
   const [selectedOption, setSelectedOption] = useState();
   const handleChange = () => {};
   const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
+    { value: "friend", option: "From a friend" },
+    { value: "online", option: "Online search" },
+    { value: "socialMedia", option: "Social media" },
+    { value: "advertisement", option: "Advertisement" },
   ];
 
   return (
     <form className="space-y-6">
-      <div className="flex gap-10">
+      <div className="flex flex-col md:flex-row gap-y-4 gap-x-10">
         <div className="w-full space-y-4">
           <input type="text" placeholder="First Name" className="w-full h-[60px] rounded-lg ring-1 ring-[#D7D7D7] px-4 outline-none" />
           <input type="text" placeholder="Phone Number" className="w-full h-[60px] rounded-lg ring-1 ring-[#D7D7D7] px-4 outline-none" />
           <input type="text" placeholder="Email address*" className="w-full h-[60px] rounded-lg ring-1 ring-[#D7D7D7] px-4 outline-none" />
           <input type="text" placeholder="Company" className="w-full h-[60px] rounded-lg ring-1 ring-[#D7D7D7] px-4 outline-none" />
         </div>
-        <div className="w-full h-full flex flex-col">
+        <div className="w-full h-full flex flex-col gap-4">
           <div className="flex-1 h-full">
             <Select>
               <SelectTrigger className="w-[180px]">
                 <SelectValue placeholder="How did you hear about us?" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
+                {options.map((option) => (
+                  <SelectItem value={option.value}>{option.option}</SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
-          <div>
+          <div className="space-y-3">
             <label htmlFor="message">Add a brisf message of what you’re interested in:</label>
             <textarea id="message" className="h-40 w-full resize-none rounded-lg ring-1 ring-[#D7D7D7] p-4 outline-none" />
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,18 @@ import { teamMembers } from "@/utils/constants";
 import Image from "next/image";
 import React from "react";
 import ContactForm from "./components/ContactForm";
+import { Menu } from "lucide-react";
 
 export default function Home() {
   return (
-    <main>
+    <main className="overflow-x-hidden">
       <div className="bg-[#F8F3F0]">
         <section>
-          <nav className="flex items-center justify-between max-w-[1620px] px-4 pt-10 md:px-10 mx-auto">
+          <nav className="flex items-center justify-between max-w-[1620px] px-4 pt-5 md:pt-10 md:px-10 mx-auto">
             <div>
               <h1 className="text-[#132742] text-3xl">Logo</h1>
             </div>
-            <div className="flex items-center gap-10">
+            <div className="hidden md:flex items-center gap-10">
               <div className="cursor-pointer">
                 <h1 className="text-lg text-[#132742]">About Us</h1>
               </div>
@@ -38,12 +39,13 @@ export default function Home() {
                 Sign Up
               </button>
             </div>
+            <Menu className="block md:hidden" />
           </nav>
         </section>
         <section>
           <div className="max-w-[1620px] mx-auto px-4 mt-28 pb-20 md:px-10">
-            <div className="flex items-center justify-between">
-              <div className="max-w-[50%] space-y-8">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-y-10">
+              <div className="md:max-w-[50%] space-y-8">
                 <h1 className="text-5xl text-[#132742] font-bold leading-[1.4] capitalize">
                   <span className="text-[#FD7D4C]">Boosting</span> potential, driving excellence!
                 </h1>
@@ -55,7 +57,7 @@ export default function Home() {
                 <button className="bg-red-orange rounded-lg h-[50px] px-6 font-semibold text-white">GET STARTED</button>
               </div>
               <div className="">
-                <Image src="/image-1.png" alt="image" width={1000} height={1000} className="max-w-[560px]" />
+                <Image src="/image-1.png" alt="image" width={1000} height={1000} className="max-w-[560px] w-full" />
               </div>
             </div>
           </div>
@@ -67,7 +69,7 @@ export default function Home() {
             <h1 className="text-[#5A687B] text-center">TRUSTED GLOBALLY</h1>
             <p className="text-xl font-semibold text-cyan-blue text-center">12,000 people trained in 86 countries</p>
           </div>
-          <div className="flex items-center mt-10 justify-between">
+          <div className="flex flex-wrap gap-y-10 items-center mt-10 justify-between">
             <div>
               <Image src="/time-warner.png" alt="time-warner" width={140} height={90} />
             </div>
@@ -83,17 +85,17 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-[#F4F8FA] mt-16 p-10 relative">
+      <section className="bg-[#F4F8FA] mt-16 py-10 relative">
         <Image alt="grid" src="/grid-crystals-orange-1.png" width={400} height={300} className="absolute right-5 top-5" />
         <div className="max-w-[1620px] mx-auto px-4 md:px-10 relative z-[1]">
           <div className="space-y-1">
             <h1 className="text-center text-red-orange">Enterprise Solutions</h1>
             <h1 className="text-[40px] text-[#132742] font-bold leading-[1.4] text-center">Empower Your Team with Enterprise Solutions</h1>
-            <p className="text-[#8993A0] text-lg text-center max-w-2xl mx-auto leading-relaxed">
+            <p className="text-[#8993A0] text-lg text-center md:max-w-2xl mx-auto leading-relaxed">
               Equip your team with our powerful enterprise solutions, designed to enhance productivity and drive business success.
             </p>
           </div>
-          <div className="grid grid-cols-3 gap-5 mt-10">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-10">
             <div className="w-full bg-white px-6 py-10 rounded-xl shadow-lg shadow-gray-200 space-y-4">
               <Image src="/teams-1.png" alt="team" width={400} height={400} className="w-full" />
               <h1 className="text-[#132742] text-2xl font-bold leading-[1.5] max-w-xs">Recruit, Train, Place - Ramp Up 360</h1>
@@ -119,9 +121,9 @@ export default function Home() {
         </div>
       </section>
       <section className="">
-        <div className="max-w-[1620px] mx-auto px-4 mt-28 pb-20 md:px-10">
-          <div className="flex items-center justify-between">
-            <div className="max-w-[50%] space-y-8">
+        <div className="max-w-[1620px] mx-auto px-4 py-14 md:px-10">
+          <div className="flex flex-col md:flex-row gap-y-10 items-center justify-between">
+            <div className="md:max-w-[50%] space-y-8">
               <h1 className="text-[40px] text-[#132742] font-bold leading-[1.4] max-w-xl">Boost Your Tech Careers with Bootcamps</h1>
               <p className="text-[#8993A0] leading-[1.7] text-lg">
                 Looking to accelerate your tech career? TalentBoost offers intensive bootcamp programs that provide hands-on training and industry-relevant
@@ -135,23 +137,23 @@ export default function Home() {
             </div>
             <div className="relative">
               <Image alt="grid" src="/grid-crystals-gray-1.png" width={500} height={300} className="absolute left-[-200px] top-[-20px] opacity-[0.15]" />
-              <Image src="/image-3.png" alt="image" width={1000} height={1000} className="max-w-[500px] relative z-[1]" />
+              <Image src="/image-3.png" alt="image" width={1000} height={1000} className="max-w-[500px] relative z-[1] w-full" />
             </div>
           </div>
         </div>
       </section>
-      <section className="bg-[#FFFCFA] mt-20 pb-20 p-10">
+      <section className="bg-[#FFFCFA] py-14">
         <div className="max-w-[1620px] mx-auto px-4 md:px-10 relative">
-          <Image alt="grid" src="/grid-crystals-orange-2.png" width={400} height={300} className="absolute left-[-100px] top-[-80px]" />
+          <Image alt="grid" src="/grid-crystals-orange-2.png" width={340} height={300} className="absolute left-[-100px]" />
           <div className=" relative z-[1]">
             <div className="space-y-1">
               <h1 className="text-center text-red-orange text-lg">Training Solutions</h1>
               <h1 className="text-[40px] text-[#132742] font-bold leading-[1.4] text-center">Empower Your Team with Training Solutions</h1>
-              <p className="text-[#8993A0] text-center max-w-2xl mx-auto leading-relaxed">
+              <p className="text-[#8993A0] text-center md:max-w-2xl mx-auto leading-relaxed">
                 Maximize your team's potential with our tailored training solutions designed to meet your organization's unique needs.
               </p>
             </div>
-            <div className="grid grid-cols-3 gap-5 mt-10">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-10">
               <div className="w-full bg-white px-6 py-10 rounded-xl shadow-lg shadow-gray-200 space-y-4">
                 <Image src="/training-1.png" alt="team" width={400} height={400} className="w-full" />
                 <h1 className="text-[#132742] text-2xl font-bold leading-[1.5] max-w-xs">Recruit, Train, Place - Ramp Up 360</h1>
@@ -178,9 +180,9 @@ export default function Home() {
         </div>
       </section>
       <section className="">
-        <div className="max-w-[1620px] mx-auto px-4 mt-28 pb-20 md:px-10">
-          <div className="flex flex-row-reverse items-center justify-between">
-            <div className="max-w-[50%] space-y-8">
+        <div className="max-w-[1620px] mx-auto px-4 py-14 md:px-10">
+          <div className="flex  flex-col md:flex-row-reverse gap-y-10 items-center justify-between">
+            <div className="md:max-w-[50%] space-y-8">
               <h1 className="text-[40px] capitalize text-[#132742] font-bold leading-[1.4] max-w-xl">
                 Discover a world of training resources and unleash your full potential
               </h1>
@@ -196,12 +198,12 @@ export default function Home() {
             </div>
             <div className="relative">
               <Image alt="grid" src="/grid-crystals-gray-2.png" width={500} height={300} className="absolute right-[-200px] top-[-20px] opacity-[0.15]" />
-              <Image src="/image-3.png" alt="image" width={1000} height={1000} className="max-w-[500px] relative z-[1]" />
+              <Image src="/image-3.png" alt="image" width={1000} height={1000} className="max-w-[500px] relative z-[1] w-full" />
             </div>
           </div>
         </div>
       </section>
-      <section className="bg-[#F4F8FA] mt-20 p-10">
+      <section className="bg-[#F4F8FA] py-14">
         <div className="max-w-[1620px] mx-auto px-4 md:px-10">
           <div className="space-y-1">
             <h1 className="text-[42px] text-[#132742] font-bold leading-[1.4] text-center">Browse Training Categories</h1>
@@ -213,15 +215,15 @@ export default function Home() {
         </div>
       </section>
       <section className="">
-        <div className="max-w-[1620px] mx-auto px-4 mt-20 pb-20 md:px-10">
-          <div className="flex items-center justify-between">
-            <div className="max-w-[50%] space-y-2">
+        <div className="max-w-[1620px] mx-auto px-4 py-14 md:px-10">
+          <div className="flex flex-col md:flex-row gap-y-10 items-center justify-between">
+            <div className="md:max-w-[50%] space-y-2">
               <h1 className="text-4xl text-[#132742] font-bold leading-[1.4] max-w-2xl">Why Company Learning</h1>
               <p className="text-cyan-blue leading-[1.7] text-2xl max-w-xl">
                 Company reputation spans the globe, having effectively trained 12,000+ people in 86 countries.
               </p>
             </div>
-            <div className="w-1/2">
+            <div className="md:w-1/2">
               <p className="text-[#8993A0] text-lg leading-relaxed">
                 Investing in company learning is essential for staying competitive in today's fast-paced business landscape. TalentBoost offers customized
                 learning solutions that empower organizations to upskill their workforce, foster innovation, and drive business growth. Stay ahead of the curve
@@ -233,10 +235,10 @@ export default function Home() {
         </div>
       </section>
       <section>
-        <div className="max-w-[1620px] mx-auto">
-          <div className="flex justify-between gap-10">
+        <div className="max-w-[1620px] mx-auto px-4 md:px-10">
+          <div className="grid grid-cols-1 min-[400px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
             {teamMembers.map((member) => (
-              <div className="team-member w-[200px] relative overflow-hidden rounded-t-2xl">
+              <div className="team-member w-[200px] min-h-[300px] relative overflow-hidden rounded-t-2xl">
                 <Image src={member.image} alt="member" width={1000} height={1000} className="h-full object-cover" />
                 <div className="absolute bottom-0 h-24 left-0 right-0 backdrop-blur-lg bg-black/30 z-[1] flex flex-col justify-center items-center">
                   <h1 className="text-xl text-white">{member.names}</h1>
@@ -247,7 +249,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-[#FFFCFA] mt-14 py-14">
+      <section className="bg-[#FFFCFA] mt-14 py-14 px-4 md:px-10">
         <div className="max-w-[1620px] mx-auto space-y-10">
           <div className="space-y-1">
             <h1 className="text-[36px] text-center text-cyan-blue">We'd love to hear from you</h1>
@@ -260,24 +262,24 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-[#132742] pt-10 pb-4 px-10">
+      <section className="bg-[#132742] pt-10 pb-4 px-5 md:px-10 ">
         <footer className="max-w-[1620px] mx-auto space-y-10">
-          <div className="flex justify-between">
+          <div className="flex flex-col md:flex-row justify-between gap-y-14">
             <div className="w-full">
               <h1 className="text-2xl text-white">Logo</h1>
               <p className="text-[#8993A0] leading-loose max-w-2xl mt-6">
                 Lorem ipsum dolor sit amet consectetur. Nunc dui massa diam nibh tellus donec volutpat sed tempus. Euismod scelerisque mattis est tortor.
               </p>
             </div>
-            <div className="space-y-6 w-1/3">
+            <div className="space-y-6 md:w-1/3">
               <h1 className="font-semibold text-white text-lg">Links</h1>
               <ul className="space-y-6">
                 <li className="text-[#D0D4D9]">Account</li>
                 <li className="text-[#D0D4D9]">Contact Us</li>
               </ul>
             </div>
-            <div className="space-y-6 w-1/3">
-              <h1 className="font-semibold text-white text-lg">Reach Us Anytime</h1>
+            <div className="space-y-6 md:w-1/3">
+              <h1 className="font-semibold text-white text-lg whitespace-nowrap">Reach Us Anytime</h1>
               <div className="flex gap-2 items-center text-[#D0D4D9]">
                 <img src="/svgs/phone.svg" className="w-6" />
                 <p>(855) 616-0999</p>
